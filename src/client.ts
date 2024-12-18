f5a98e938726cfa7ea72787b90929a86bc842a4f
Frederick Akuffo <kofiakuffoboafo@gmail.com>
2024-12-18 14:20:50 +0000

    base url configured to local

--- src/client.ts
+++ src/client.ts
@@ -2,13 +2,14 @@ import { browser } from '$app/environment';
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'https://dyslexia-mgt-api-auhxg4bpcrekdyhc.uksouth-01.azurewebsites.net/api',
+    baseURL: 'http://127.0.0.1:8000/api',
     timeout: 30000,
 });
 
 if (browser) {
     const token = localStorage?.getItem('t');
     if(token) {
+        axiosInstance.defaults.headers.common['Content-Type'] = 'application/json';
         axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
     }
 } 
@@ -17,6 +18,7 @@ axiosInstance.interceptors.request.use(
     (config) => {
       const token = localStorage.getItem('t');
       if (token) {
+        config.headers['Content-Type'] = 'application/json';
         config.headers['Authorization'] = `Bearer ${token}`;
       }
       return config;
